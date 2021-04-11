75e544c7758191af56ffe33ef926bee59bfb9c9c
hawk_tomy <hawk201000211101@gmail.com>
2021-04-11 23:12:23 +0900

    fix

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     include_package_data=True,
     install_requires=[
         'pyyaml',
-        'git+https://github.com/Rapptz/discord-ext-menus',
+        'discord-ext-menus',
         ],
 )
