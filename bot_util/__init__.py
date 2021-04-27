6b3d622854b30407e47ddd6ec7fd372bb0e38b58
tomy <67221751+hawk-tomy@users.noreply.github.com>
2021-04-28 08:22:33 +0900

    __init__.py add yaml config

--- bot_util/__init__.py
+++ bot_util/__init__.py
@@ -1,4 +1,11 @@
 import logging
 
 
+YAML_DUMP_CONFIG = {
+    'encoding':'utf8',
+    'allow_unicode':True,
+    'default_flow_style':False
+    }
+
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
