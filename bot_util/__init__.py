9734f451f4d0518050f63af5a28384aa4c403500
hawk_tomy <hawk201000211101@gmail.com>
2021-05-30 01:20:04 +0900

    add wraped_embed.Embed alias.

--- bot_util/__init__.py
+++ bot_util/__init__.py
@@ -3,7 +3,10 @@ import logging
 
 from . import util
 from .util import *
+from .wraped_embed import Embed
 
 
-__all__ = util.__all__
+__all__ = util.__all__ + (
+    'Embed',
+)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
