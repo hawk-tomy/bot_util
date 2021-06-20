0bc1edb14f12e933811f469f92c6281b3d462898
hawk_tomy <hawk201000211101@gmail.com>
2021-06-21 01:02:00 +0900

    update dispander. updated to https://github.com/DiscordBotPortalJP/dispander/commit/fc443656ce05e7370510cfd7f500992bfafdd502

--- bot_util/dispander.py
+++ bot_util/dispander.py
@@ -1,17 +1,17 @@
 """
 MIT License
-
 Copyright (c) 2019 - 2021 1ntegrale9
-
 https://github.com/DiscordBotPortalJP/dispander
 """
 from __future__ import annotations
 
 
+import os
 import re
 
 
 from discord import Embed
+from discord.embeds import EmptyEmbed
 
 
 from . import _bot_config
@@ -25,13 +25,52 @@ regex_discord_message_url = (
     '(?!<)https://(ptb.|canary.)?discord(app)?.com/channels/'
     '(?P<guild>[0-9]{18})/(?P<channel>[0-9]{18})/(?P<message>[0-9]{18})(?!>)'
 )
+regex_extra_url = (
+    r'\?base_aid=(?P<base_author_id>[0-9]{18})'
+    '&aid=(?P<author_id>[0-9]{18})'
+    '&extra=(?P<extra_messages>(|[0-9,]+))'
+)
+DELETE_REACTION_EMOJI = os.environ.get("DELETE_REACTION_EMOJI", "\U0001f5d1")
+
+
+async def delete_dispand(bot, payload):
+    if str(payload.emoji) != DELETE_REACTION_EMOJI:
+        return
+    if payload.user_id == bot.user.id:
+        return
+
+    channel = bot.get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    await _delete_dispand(bot, message, payload.user_id)
+
+
+async def _delete_dispand(bot, message, operator_id):
+    if message.author.id != bot.user.id:
+        return
+    elif not message.embeds:
+        return
+
+    embed = message.embeds[0]
+    if getattr(embed.author, "url", None) is None:
+        return
+    data = from_jump_url(embed.author.url)
+    if not (data["base_author_id"] == operator_id or data["author_id"] == operator_id):
+        return
+    await message.delete()
+    for message_id in data["extra_messages"]:
+        extra_message = await message.channel.fetch_message(message_id)
+        if extra_message is not None:
+            await extra_message.delete()
 
 
 async def dispand(message):
     messages = await extract_message(message)
     for m in messages:
+        sent_messages = []
+
         if m.content or m.attachments:
-            await message.channel.send(embed=compose_embed(m))
+            sent_message = await message.channel.send(embed=compose_embed(m))
+            sent_messages.append(sent_message)
         # Send the second and subsequent attachments with embed (named 'embed') respectively:
         for attachment in m.attachments[1:]:
             embed = Embed(
@@ -39,9 +78,23 @@ async def dispand(message):
             ).set_image(
                 url=attachment.proxy_url
             )
-            await message.channel.send(embed=embed)
+            sent_attachment_message = await message.channel.send(embed=embed)
+            sent_messages.append(sent_attachment_message)
+
         for embed in m.embeds:
-            await message.channel.send(embed=embed)
+            sent_embed_message = await message.channel.send(embed=embed)
+            sent_messages.append(sent_embed_message)
+
+        # 一番先頭のメッセージにゴミ箱のリアクションをつける
+        main_message = sent_messages.pop(0)
+        await main_message.add_reaction(DELETE_REACTION_EMOJI)
+        main_embed = main_message.embeds[0]
+        main_embed.set_author(
+            name=getattr(main_embed.author, "name", EmptyEmbed),
+            icon_url=getattr(main_embed.author, "icon_url", EmptyEmbed),
+            url=make_jump_url(message, m, sent_messages)
+        )
+        await main_message.edit(embed=main_embed)
 
 
 async def extract_message(message):
@@ -64,6 +117,39 @@ async def fetch_message_from_id(guild, channel_id, message_id):
     return message
 
 
+def make_jump_url(base_message, dispand_message, extra_messages):
+    """
+    make jump url which include more information
+    :param base_message: メッセージリンクが貼られていたメッセージ
+    :param dispand_message: 展開中のメッセージ
+    :param extra_messages: 展開する際にでた二つ目以降のメッセージ(e.g. 画像やembed)
+    :return: 混入が完了したメッセージリンク
+    """
+    # base_aid: メッセージリンクで飛べる最初のメッセージの送信者のid
+    # aid: メッセージリンクを送信したユーザーのid
+    return "{0.jump_url}?base_aid={1.id}&aid={2.id}&extra={3}".format(
+        dispand_message,
+        dispand_message.author,
+        base_message.author,
+        ",".join([str(i.id) for i in extra_messages])
+    )
+
+
+def from_jump_url(url):
+    """
+    メッセージリンクから情報を取得します。
+    :param url: メッセージリンク
+    :return: dict
+    """
+    base_url_match = re.match(regex_discord_message_url + regex_extra_url, url)
+    data = base_url_match.groupdict()
+    return {
+        "base_author_id": int(data["base_author_id"]),
+        "author_id": int(data["author_id"]),
+        "extra_messages": [int(_id) for _id in data["extra_messages"].split(",")] if data["extra_messages"] else []
+    }
+
+
 def compose_embed(message):
     embed = Embed(
         description=message.content,
@@ -73,6 +159,7 @@ def compose_embed(message):
     embed.set_author(
         name=message.author.display_name,
         icon_url=message.author.avatar_url,
+        url=message.jump_url
     )
     embed.set_footer(
         text=message.channel.name,
